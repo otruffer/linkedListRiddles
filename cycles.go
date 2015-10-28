b389406d1a0a3a147b90e0bd016ea49c07bc99f1
Oskar Truffer <oskar.truffer@studer-raimann.ch>
2015-10-28 10:36:11 +0100

    solution to cycles.

--- cycles.go
+++ cycles.go
@@ -4,6 +4,17 @@ package linkedListRiddles
 // Your goal is to detect if the linked list contains cycles! Furthermore you must
 // NOT alter the list in any way, your memory usage should be INDEPENDENT of the list's
 // length (so no keeping track of all visited nodes) and should run in O(n)... so linear time
-func HasCycle(head Node) bool {
-	return true
+func HasCycle(head *Node) bool {
+	var drag = head
+	for i := 0; head.hasNext(); i++ {
+		if i%2 == 1 {
+			drag = drag.next
+		}
+		head = head.next
+		if drag == head {
+			return true
+		}
+	}
+
+	return false
 }
--- solution_test.go
+++ solution_test.go
@@ -7,18 +7,18 @@ import (
 func TestCycles(t *testing.T) {
 	var n1, n2, n3, n4, n5 Node
 
-	if HasCycle(n1) {
+	if HasCycle(&n1) {
 		t.Error("A list with a single element does not have cycles...")
 	}
 
 	n1.next = &n2
-	if HasCycle(n1) {
+	if HasCycle(&n1) {
 		t.Error("n1 -> n2 -> nil. Does not have cylcles.")
 	}
 
 	n1.next = &n2
 	n2.next = &n1
-	if !HasCycle(n1) {
+	if !HasCycle(&n1) {
 		t.Error("n1 -> n2 -> n1. Does have cylces.")
 	}
 
@@ -27,13 +27,13 @@ func TestCycles(t *testing.T) {
 	n3.next = &n4
 	n4.next = &n5
 	n5.next = &n3
-	if !HasCycle(n1) {
+	if !HasCycle(&n1) {
 		t.Error("n1 -> n2 -> n3 -> n4 -> n5 -> n3. Does have cylces.")
 	}
 
 	n5.next = nil
 
-	if HasCycle(n1) {
+	if HasCycle(&n1) {
 		t.Error("n1-> n2->n3->n4->n5->nil does not have cycles")
 	}
 
